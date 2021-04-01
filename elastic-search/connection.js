673a61fdcb474f5ddfc05273834c702f6cd3db70
azad1947 <azad00001947@gmail.com>
2021-04-02 01:06:35 +0530

    added a check for the es index

--- elastic-search/connection.js
+++ elastic-search/connection.js
@@ -1,30 +1,38 @@
 import elastic from 'elasticsearch';
 import movies from '../data/csvjson.js';
+import _ from 'lodash';
 
 
 export const client = new elastic.Client({
     host: 'http://localhost:9200'
 })
 
-export const insert_data_to_elastic_search = () => {
+export const insert_data_to_elastic_search = async () => {
+
+    const index_exists = await client.indices.exists({ index: 'movies' });
+
+    if (!index_exists) {
+        client.indices.create({
+            index: 'movies'
+        });
+    }
+
     client.count({ index: 'movies' })
-        .then(result => {
+        .then(async (result) => {
             if (result.count) {
                 console.log('data found in elastic search.');
             } else {
                 console.log('inserting data to elastic search.');
-                client.indices.create({
-                    index: 'movies'
-                });
-                data_feeding();
+                await data_feeding();
             }
         })
         .catch(err => {
             console.log('error--> ', err.message);
         })
+
 }
 
-const data_feeding = () => {
+const data_feeding = async () => {
     const bulk = _.reduce(movies, (acc, movie) => {
         acc.push({ index: { _index: 'movies', _type: 'movie' } });
         acc.push(movie);
